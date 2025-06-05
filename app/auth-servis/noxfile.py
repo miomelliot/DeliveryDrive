21a060a0454474c539d1276254ab84289d7d2aa3
miomelliot <s89652158910@gmail.com>
2025-06-05 19:19:50 +0300

    wip

--- app/auth-servis/noxfile.py
+++ app/auth-servis/noxfile.py
@@ -4,17 +4,13 @@ nox.options.default_venv_backend = "uv|virtualenv"
 nox.options.sessions = ["ruff", "ruff_format", "mypy"]
 
 
-@nox.session(
-    reuse_venv=True,
-)
+@nox.session(reuse_venv=True)
 def ruff(session: nox.Session) -> None:
     session.install(".[dev]")
     session.run("ruff", "check", ".", external=True)
 
 
-@nox.session(
-    reuse_venv=True,
-)
+@nox.session(reuse_venv=True)
 def ruff_format(session: nox.Session) -> None:
     session.install(".[dev]")
     session.run("ruff", "format", ".", external=True)
@@ -31,3 +27,9 @@ def mypy(session: nox.Session) -> None:
 def tests(session: nox.Session) -> None:
     session.install(".[dev]")
     session.run("pytest", "tests/", external=True)
+
+
+@nox.session(reuse_venv=True)
+def serve(session: nox.Session) -> None:
+    session.install(".")
+    session.run("uvicorn", "src.main:app", "--host", "0.0.0.0", "--port", "8000", external=True)
