76d84c499c3fa1a6f260b31131cc0dca42da76cb
miomelliot <s89652158910@gmail.com>
2025-06-15 21:25:11 +0300

    wip: osrm-service

--- app/osrm-service/src/core/security.py
+++ app/osrm-service/src/core/security.py
@@ -2,20 +2,10 @@
 from typing import Union, cast
 
 from jose import JWTError, jwt
-from passlib.context import CryptContext
 
 from src.core.config import Settings, get_settings
 
 _cfg: Settings = get_settings()
-_pwd_ctx = CryptContext(schemes=["argon2"], deprecated="auto")
-
-
-def hash_password(password: str) -> str:
-    return cast(str, _pwd_ctx.hash(password))
-
-
-def verify_password(password: str, password_hash: str) -> bool:
-    return cast(bool, _pwd_ctx.verify(password, password_hash))
 
 
 def decode_access_token(token: str) -> dict[str, Union[str, int]]:
