aa6e5f82430102629aa5e3a2ef588d2844f17f67
miomelliot <s89652158910@gmail.com>
2025-06-16 04:18:25 +0300

    wip: osrm-service

---
+++ app/osrm-service/src/schemas/logistics.py
@@ -0,0 +1,40 @@
+# src/schemas/logistics.py
+from datetime import time
+from typing import Literal
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+
+from src.schemas.address import AddressRead
+
+
+class Solver(BaseModel):
+    max_runtime_sec: int = 30
+    num_solutions: int = 1
+    allow_waiting: bool = True
+
+
+class TransportType(BaseModel):
+    name: str
+    avg_speed: float
+    capacity: float
+
+
+class Create(BaseModel):
+    time_window: list[time]
+    transport_type: TransportType
+
+
+class Order(BaseModel):
+    order_id: UUID
+    address: AddressRead
+    weight: float
+    service_duration: int = Field(default=1500)
+    time_window: list[time]
+
+
+class Logistics(BaseModel):
+    orders: list[Order]
+    creates: list[Create]
+    solver: Solver
+    osrm_profile: Literal["driving" , "foot" , "bike"]  = "driving"
